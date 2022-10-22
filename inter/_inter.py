b4394a9b44c010271927361d9e952e491f0f5c3a
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-10-22 11:27:58 -0300

    Add Inter.get_balance

--- inter/_inter.py
+++ inter/_inter.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from inter._client import Client
 
 
@@ -8,3 +10,15 @@ class Inter:
     @classmethod
     def from_credentials(cls, client_id, client_secret, cert_path, key_path):
         return cls(client=Client(client_id, client_secret, cert_path, key_path))
+
+    def get_balance(self, date=None):
+        """
+        Retorna o saldo disponível da conta em determinado dia.
+
+        :param date: data do saldo
+        :type date: :class:`datetime.date`
+
+        :return: saldo disponível
+        :rtype: :class:`decimal.Decimal`
+        """
+        return Decimal(str(self._client.get_balance(date)['disponivel']))
--- tests/test_inter.py
+++ tests/test_inter.py
@@ -1,3 +1,5 @@
+from decimal import Decimal
+
 from inter import Inter
 from inter._client import Client
 
@@ -19,3 +21,23 @@ def test_from_credentials(faker, mocker):
 
     assert isinstance(inter._client, Client)
     client_mock.assert_called_once_with(client_id, client_secret, cert_path, key_path)
+
+
+def test_get_balance(faker, mocker, balance_data):
+    client = mocker.MagicMock(spec=Client)
+    client.get_balance.return_value = balance_data
+
+    inter = Inter(client)
+
+    assert inter.get_balance() == Decimal(str(balance_data['disponivel']))
+
+
+def test_get_balance_with_date(faker, mocker, balance_data):
+    client_mock = mocker.MagicMock(spec=Client)
+    client_mock.get_balance.return_value = balance_data
+    date = faker.past_date()
+
+    inter = Inter(client_mock)
+    inter.get_balance(date)
+
+    client_mock.get_balance.assert_called_once_with(date)
