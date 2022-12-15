3c1255e9ef871dedccff40391ef8291060315c13
Lucas Rangel Cezimbra <lucas.cezimbra@gmail.com>
2022-12-14 22:59:34 -0300

    Fix version

--- inter/__init__.py
+++ inter/__init__.py
@@ -3,4 +3,4 @@ from inter._inter import Inter, Operation, Payment  # noqa
 
 __author__ = """Lucas Rangel Cezimbra"""
 __email__ = 'lucas@cezimbra.tec.br'
-__version__ = '0.0.6'
+__version__ = '0.0.7'
